f3e21e9bee7d24e9e49e298ab747940f20a7004f
Student User <student@workstation.lab.example.com>
2020-03-05 23:12:11 +0000

    add newapp command and finaltodo yaml

---
+++ review-todoapp.sh
@@ -0,0 +1,1 @@
+oc new-app yangd2019-review-common/todoapp -p PASSWORD=redhat -p CLEAN_DATABASE=false -p HOSTNAME=frontend-yangd2019-review-dockerfile.apps.ocp-eu2.prod.nextcle.com -p BACKEND=backend-yangd2019-review-service.yangd2019.apps.ocp-eu2.prod.nextcle.com
